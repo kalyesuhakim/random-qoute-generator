db7dbe2f22d398142df20c0ced93f48e568b77d2
Kalyesubula Hakim <kalyesuhakim54@gmail.com>
2021-04-05 10:47:48 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+export default class componentName extends Component {
+  state ={ advice: ""}
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) =>{
+      const  advice  = response.data.slip.advice;
+      this.setState({ advice })
+
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    const { advice } = this.state
+    return (
+      <div className="app">
+         <div className="card">
+           <h1 className="heading">
+             {advice}
+           </h1>
+           <button className="button" onClick={this.fetchAdvice}>
+             <span>GIVE ME ADVICE</span>
+           </button>
+         </div>
+      </div>
+    )
+  }
+}
